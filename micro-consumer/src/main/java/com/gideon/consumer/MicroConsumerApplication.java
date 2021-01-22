1be241d9607a54242952f33a2fa2695f7c10052b
maoqian0118@163.com <a123456789>
2021-01-22 17:29:43 +0800

    整合Nacos

--- micro-consumer/src/main/java/com/gideon/consumer/MicroConsumerApplication.java
+++ micro-consumer/src/main/java/com/gideon/consumer/MicroConsumerApplication.java
@@ -3,7 +3,9 @@ package com.gideon.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroConsumerApplication {
     public static void main(String[] args) {
--- micro-provider/src/main/java/com/gideon/provider/MicroProviderApplication.java
+++ micro-provider/src/main/java/com/gideon/provider/MicroProviderApplication.java
@@ -3,7 +3,9 @@ package com.gideon.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroProviderApplication {
     public static void main(String[] args) {
