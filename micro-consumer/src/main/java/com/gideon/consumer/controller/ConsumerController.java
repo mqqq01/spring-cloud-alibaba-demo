ecc9da2a2a47cb6b88d3fa254dc59e410da52a4b
maoqian0118@163.com <a123456789>
2021-01-25 16:40:10 +0800

    整合Nacos success

--- micro-consumer/src/main/java/com/gideon/consumer/controller/ConsumerController.java
+++ micro-consumer/src/main/java/com/gideon/consumer/controller/ConsumerController.java
@@ -1,13 +1,29 @@
 package com.gideon.consumer.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ConsumerController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/consumer")
     public String consumer(){
         return "hello consumer";
+    }
+
+    @GetMapping("/discoveryClient")
+    public String discoveryClient(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("micro-provider");
+
+        return JSONObject.toJSONString(instances);
     }
 }
